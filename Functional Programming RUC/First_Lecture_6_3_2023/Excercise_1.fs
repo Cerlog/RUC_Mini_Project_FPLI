b6f10b794d7db2a75ae4c31a0f16e2cd4cef41cd
Petr B Nylander <petrboska@gmail.com>
2023-03-07 09:29:44 +0100

    Excercise 1

---
+++ Functional Programming RUC/First_Lecture_6_3_2023/Excercise_1.fs
@@ -0,0 +1,37 @@
+﻿(* Excercise 1 *)
+
+// a
+"Hello world";;
+// val it: string = "Hello world"
+
+// b
+Hello world
+// error FS0039: The value or constructor 'Hello' is not defined.
+
+// c
+3 + 4;;
+//val it: int = 7
+
+// d
+3 + four;;
+// The value or constructor 'four' is not defined.
+
+// e
+3 + "four";;
+// The type 'string' does not match the type 'int'
+
+// f
+3 + 4.2;;
+// The type 'float' does not match the type 'int'
+
+// g
+4.2 + 3;;
+// The type 'int' does not match the type 'float'
+
+// h
+4.2 + 3.0;;
+// val it: float = 7.2
+
+// i
+"horse" + "shoe";;
+//val it: string = "horseshoe"
---
+++ Functional Programming RUC/First_Lecture_6_3_2023/poly.fs
@@ -0,0 +1,37 @@
+﻿module poly
+
+
+(* Excercise 2 *)
+
+// a
+// binding the pair of constants to variable f
+//let f(x) = 2.0 * x**2 + 4.0*x -1.0;;
+let f = (2.0, 4.0, -1.0)
+
+// b
+// vertex of the 2nd degree polynomial
+let vertex (a, b, c) = (-b / (2.0 * a), -b * b / (4.0 * a) + c);;
+
+
+
+// c
+let solve (a, b, c) =
+    let d = b**2.0 - 4.0 * a * c
+    let x1 = (-b - sqrt(d)) / 2.0 * a
+    let x2 = (-b + sqrt(d)) / 2.0 * a
+    (x1, x2)
+    
+    
+(*let solveSqrt (a, b, c) =
+    let sqrtD = sqrt(b**2.0 - 4.0 * a * c)
+    let x1 = (-b - sqrtD) / 2.0 * a
+    let x2 = (-b + sqrtD) / 2.0 * a
+    (x1, x2)*)
+    
+let solve2 (a, b, c) = let d = sqrt(b**2.0 - 4.0 * a * c) in ((-b - sqrt(d) / 2.0 * a), (-b + sqrt(d)) / 2.0 * a)
+    
+
+
+// d
+let eval (a, b, c) (x: float) = a * x**2 + b * x + c;;
+
